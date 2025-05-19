b06d690fb7b1625d462378007407bafecb2d4ef2
Evo512 <justindoggett.jrd@gmail.com>
2025-05-19 13:21:00 -0500

    Add files via upload

---
+++ Story Maker.js
@@ -0,0 +1,2 @@
+//This is where you can write your story and embed the variables from the story maker.
+storyResult.innerHTML = `Every ${season} season, my ${parent} and I in the ${time} will take on ${action}. After our activities we will enjoy having ${food} food.`;
